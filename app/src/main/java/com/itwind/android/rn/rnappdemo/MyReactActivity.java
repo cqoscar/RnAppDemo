b85649bb2351ff762975e135a02fa29c87080bc6
cqoscar <heshijun_009@foxmail.com>
2017-05-13 18:02:14 +0800

    windows系统上搭建android应用集成React native 完成集成

---
+++ app/src/main/java/com/itwind/android/rn/rnappdemo/MyReactActivity.java
@@ -0,0 +1,19 @@
+package com.itwind.android.rn.rnappdemo;
+
+import com.facebook.react.ReactActivity;
+
+import javax.annotation.Nullable;
+
+/**
+ * Created by oscar on 2017/5/13.
+ */
+
+public class MyReactActivity extends ReactActivity {
+
+    @Nullable
+    @Override
+    protected String getMainComponentName() {
+
+        return "ItWindRN";
+    }
+}
